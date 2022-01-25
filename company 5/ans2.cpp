1484916c6b9a9475d1b96d3dfd7f3b77d72bf024
Nidhi Gupta <nidhigupta7983@gmail.com>
2022-01-25 19:43:53 +0530

    #arsh

---
+++ company 5/ans2.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+public:
+    bool isWordExist(vector<vector<char>>& board, string word) {
+        // Code here
+        int m=board.size();
+        int n=board[0].size();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]==word[0])
+                {
+                    if(dfs(board,word,i,j,0))
+                    return true;
+                }
+            }
+        }
+        return false;
+    
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<char>>board(n, vector<char>(m, '*'));
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < m; j++)
+				cin >> board[i][j];
+		string word;
+		cin >> word;
+		Solution obj;
+		bool ans = obj.isWordExist(board, word);
+		if(ans)
+			cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+}  // } Driver Code Ends
